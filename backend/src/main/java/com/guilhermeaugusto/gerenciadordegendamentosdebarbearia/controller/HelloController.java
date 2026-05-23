ee41699cc74f93561e35bd00e7919175f6c57d9c
Guilherme Augusto <guibaaugust067@gmail.com>
2026-05-23 16:45:48 -0300

    chore: organize backend packages

--- backend/src/main/java/com/guilhermeaugusto/gerenciadordegendamentosdebarbearia/controller/HelloController.java
+++ backend/src/main/java/com/guilhermeaugusto/gerenciadordegendamentosdebarbearia/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.guilhermeaugusto.gerenciadordegendamentosdebarbearia;
+package com.guilhermeaugusto.gerenciadordegendamentosdebarbearia.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
